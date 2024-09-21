f30cc7aeda5c4e778453ecb96f331da571a0820a
SGNM97 <92959734+SGNM97@users.noreply.github.com>
2024-09-21 15:21:20 -0300

    Add files via upload

---
+++ src/main/java/com/example/inicial1/entities/audit/Revision.java
@@ -0,0 +1,33 @@
+package com.example.inicial1.entities.audit;
+
+import jakarta.persistence.*;
+import org.hibernate.envers.RevisionEntity;
+import org.hibernate.envers.RevisionNumber;
+import org.hibernate.envers.RevisionTimestamp;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import com.example.inicial1.config.CustomRevisionListener;
+
+@Entity
+@Table(name="REVISION_INFO")
+@RevisionEntity(CustomRevisionListener.class)
+@Data
+public class Revision implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="revision_seq")
+    @SequenceGenerator(
+            name="revision_seq",
+            sequenceName="rbac.seq_revision_id"
+    )
+    @RevisionNumber
+    private int id;
+
+    @Column(name="REVISION_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    @RevisionTimestamp
+    private Date date;
+}
